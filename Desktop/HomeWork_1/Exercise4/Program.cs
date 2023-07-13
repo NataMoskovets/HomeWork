88a8bb3c5193fd1bcb20e8ddb729a8694e76c9cf
Nataly <tuchka86@mail.ru>
2023-07-13 21:58:35 +0500

    Добавили решение для задания № 4

---
+++ Desktop/HomeWork_1/Exercise4/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+Console.WriteLine("Введите перовое число: ");
+int numberA = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int numberB = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите третье число: ");
+int numberC = int.Parse(Console.ReadLine());
+if (numberA > numberB & numberA > numberC)
+{
+    Console.WriteLine($"Число {numberA} является максимальным из трёх чисел");
+}
+else
+{
+    if (numberB > numberA & numberB > numberC)
+    {
+        Console.WriteLine($"Число {numberB} является максимальным из трёх чисел");
+    }
+    else
+    {
+        if (numberC > numberA & numberC > numberB)
+        {
+            Console.WriteLine($"Число {numberC} является максимальным из трёх чисел");
+        }
+    }
+}
